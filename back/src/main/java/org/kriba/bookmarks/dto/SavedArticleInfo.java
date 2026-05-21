8c24d6bee142cbf502ce5b4addfbff55332ece6b
Roberto Lucas <lucascastilloroberto@gmail.com>
2026-05-21 08:16:27 +0200

    Rename SavedArticleInfo to ArticleResponseDto

--- back/src/main/java/org/kriba/bookmarks/dto/SavedArticleInfo.java
+++ back/src/main/java/org/kriba/bookmarks/dto/SavedArticleInfo.java
@@ -1,8 +1,7 @@
 package org.kriba.bookmarks.dto;
 
 import lombok.Builder;
-
 import java.time.LocalDateTime;
+
 @Builder
-public record SavedArticleInfo(Long id, String externalArticleId, String title, String url, LocalDateTime timeStamp) {
-}
+public record ArticleResponseDto(Long id, String externalArticleId, String title, String url, LocalDateTime timeStamp) {}
