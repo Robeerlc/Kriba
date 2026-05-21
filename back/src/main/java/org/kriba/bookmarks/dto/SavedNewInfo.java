180ef836819690bcc985a9dad859c4f33f6f4d87
Roberto Lucas <lucascastilloroberto@gmail.com>
2026-05-21 08:17:10 +0200

    Rename SavedNewInfo to ArticleInputDto

--- back/src/main/java/org/kriba/bookmarks/dto/SavedNewInfo.java
+++ back/src/main/java/org/kriba/bookmarks/dto/SavedNewInfo.java
@@ -1,4 +1,3 @@
 package org.kriba.bookmarks.dto;
 
-public record SavedNewInfo(String externalArticleId, String title, String url, String category) {
-}
+public record ArticleInputDto(String externalArticleId, String title, String url, String category) {}
