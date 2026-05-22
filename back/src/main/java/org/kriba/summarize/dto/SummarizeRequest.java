046327f090172750e02741bd856195c18dba8451
GuillermoDelgado24 <guillermo.delgado545@gmail.com>
2026-05-22 11:07:28 +0200

    Delete back/src/main/java/org/kriba/summarize/dto/SummarizeRequest.java

--- back/src/main/java/org/kriba/summarize/dto/SummarizeRequest.java
+++
@@ -1,6 +0,0 @@
-package org.kriba.summarize.dto;
-
-import org.kriba.users.dto.LoginRequest;
-
-public record SummarizeRequest(LoginRequest loginRequest, String textContent, String articleUrl, String category) {
-}
