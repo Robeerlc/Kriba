60738fe12354757d62fa082b22a4e7f848107b2b
Roberto Lucas <lucascastilloroberto@gmail.com>
2026-05-22 12:02:15 +0200

    Change suscription endpoint request

--- back/src/main/java/org/kriba/subscriptions/controller/SubscriptionController.java
+++ back/src/main/java/org/kriba/subscriptions/controller/SubscriptionController.java
@@ -4,6 +4,7 @@ import org.kriba.subscriptions.dto.AuthSubscription;
 import org.kriba.subscriptions.dto.SubscriptionList;
 import org.kriba.subscriptions.service.SubscriptionsService;
 import org.kriba.users.dto.AuthResponse;
+import org.kriba.users.dto.LoginRequest;
 import org.kriba.users.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,12 +36,11 @@ public class SubscriptionController {
 	}
 
 	@PostMapping("/list")
-	public ResponseEntity<SubscriptionList> listSubscriptions(@RequestBody AuthSubscription request) {
-		if (request == null || request.loginRequest() == null) {
+	public ResponseEntity<SubscriptionList> listSubscriptions(@RequestBody LoginRequest request) {
+		if (request == null) {
 			return ResponseEntity.badRequest().build();
 		}
-		AuthResponse authResponse = userService.login(request.loginRequest());
+		AuthResponse authResponse = userService.login(request);
 		return ResponseEntity.ok(subscriptionService.getSubscriptions(authResponse.userId()));
 	}
 }
-
