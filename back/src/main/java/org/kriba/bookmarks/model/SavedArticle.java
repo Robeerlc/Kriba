99cbc4915abe29d89b0a472acef90049be7b1a11
Roberto Lucas <lucascastilloroberto@gmail.com>
2026-05-22 11:32:43 +0200

    Refactor SavedArticle fields with column definitions
    Updated column definitions for description, content, url, and image fields to specify TEXT type and length constraints.

--- back/src/main/java/org/kriba/bookmarks/model/SavedArticle.java
+++ back/src/main/java/org/kriba/bookmarks/model/SavedArticle.java
@@ -1,7 +1,15 @@
 package org.kriba.bookmarks.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
@@ -26,16 +34,16 @@ public class SavedArticle {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(columnDefinition = "TEXT", nullable = false)
     private String description;
 
-    @Column(nullable = false)
+    @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
-    @Column(nullable = false)
+    @Column(length = 1000, nullable = false)
     private String url;
 
-    @Column(nullable = false)
+    @Column(length = 1000, nullable = false)
     private String image;
 
     @Column(nullable = false)
