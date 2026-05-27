19aa349536e78a671130146b2ba41fae7d98065b
Roberto Lucas <lucascastilloroberto@gmail.com>
2026-05-27 11:04:59 +0200

    Simplify method signature in SubscriptionRepository

--- back/src/main/java/org/kriba/subscriptions/repository/SubscriptionRepository.java
+++ back/src/main/java/org/kriba/subscriptions/repository/SubscriptionRepository.java
@@ -2,15 +2,10 @@ package org.kriba.subscriptions.repository;
 
 import org.kriba.subscriptions.model.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
     List<Subscription> findAllByUserId(long userId);
-    Optional<Subscription> findByUserIdAndExternalSourceIdAndSourceName(
-            long userId,
-            String externalSourceId,
-            String sourceName
-    );
+    Optional<Subscription> findByUserIdAndExternalSourceId(long userId, String externalSourceId);
 }
