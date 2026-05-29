42531773524a13e71dec7ee3d8e7fcfd4dccc245
GuillermoDelgado24 <guillermo.delgado545@gmail.com>
2026-05-29 12:15:19 +0200

    Paginacion del Feed

--- back/src/main/java/org/kriba/news/service/NewsInfoService.java
+++ back/src/main/java/org/kriba/news/service/NewsInfoService.java
@@ -74,29 +74,46 @@ public class NewsInfoService {
 
         FeedCache feedCache = getFeedCache(cacheKey);
         List<NewsInfo> articles = new ArrayList<>(feedCache.articles());
-        int pageSize = pageable.getPageSize();
-        int start;
 
+        if(pageable.getPageNumber() == 0 && feedCache.lastDeliveredPublishedAt() != null){
+            List<NewsInfo> olderBath = loadOlderBatch(userId, category, FEED_BATCH_SIZE, feedCache.lastDeliveredPublishedAt());
+            List<NewsInfo> cleanOlderBatch = removeAlreadyCachedArticles(articles, olderBath);
+            if(cleanOlderBatch.isEmpty()){
+                return Page.empty(pageable);
+            }
 
-        if(pageable.getPageNumber() == 0 && feedCache.lastDeliveredIndex() != null){
-            start = feedCache.lastDeliveredIndex() + 1;
-        }else{
-            start = (int) pageable.getOffset();
+            articles.addAll(cleanOlderBatch);
+            articles = cleanAndSortArticles(articles);
+
+            List<NewsInfo> content = cleanOlderBatch.size() > pageable.getPageSize()
+                    ? cleanOlderBatch.subList(0 , pageable.getPageSize()): cleanOlderBatch;
+
+            String lastDeliveredPublishedAt = content.getLast().getPublishedAt();
+
+            saveFeedCache(cacheKey, articles, null, lastDeliveredPublishedAt);
+
+            return new PageImpl<>(content, pageable,content.size());
         }
-        int requiredEnd = start + pageSize;
-        articles = getOrExpandCachedFeed(userId, category,cacheKey,requiredEnd);
 
-        if(articles.isEmpty() || start >= articles.size()){
-            saveFeedCache(cacheKey, articles, feedCache.lastDeliveredIndex());
-            return Page.empty(pageable);
+        int requiredEnd = (int) pageable.getOffset() + pageable.getPageSize();
+
+        articles = getOrExpandCachedFeed(userId, category, cacheKey, requiredEnd);
+
+        Page<NewsInfo> page = paginate(articles, pageable);
+
+        if (!page.getContent().isEmpty()) {
+            List<NewsInfo> content = page.getContent();
+            NewsInfo lastArticle = content.getLast();
+
+            saveFeedCache(
+                    cacheKey,
+                    articles,
+                    (int) pageable.getOffset() + content.size() - 1,
+                    lastArticle.getPublishedAt()
+            );
         }
 
-        int end = Math.min(requiredEnd, articles.size());
-        List<NewsInfo> content = articles.subList(start,end);
-        int newLastDeliveredIndex = end - 1;
-        saveFeedCache(cacheKey,articles,newLastDeliveredIndex);
-
-        return new PageImpl<>(content,pageable,articles.size());
+        return page;
     }
     public List<NewsInfo> getOrExpandCachedFeed(Long userId, String category, String cacheKey, int requiredEnd){
         FeedCache feedCache = getFeedCache(cacheKey);
@@ -254,29 +271,29 @@ public class NewsInfoService {
         Cache cache = cacheManager.getCache("generalFeed");
 
         if(cache == null){
-            return new FeedCache(new ArrayList<>(), null, null,null);
+            return new FeedCache(new ArrayList<>(), null, null,null,null);
         }
         FeedCache feedCache = cache.get(cacheKey, FeedCache.class);
         if(feedCache == null || feedCache.articles() == null){
-            return new FeedCache(new ArrayList<>(),null,null,null);
+            return new FeedCache(new ArrayList<>(),null,null,null,null);
         }
 
-        return new FeedCache(new ArrayList<>(feedCache.articles()),feedCache.newestPublishedAt(),feedCache.oldestPublishedAt(),feedCache.lastDeliveredIndex());
+        return new FeedCache(new ArrayList<>(feedCache.articles()),feedCache.newestPublishedAt(),feedCache.oldestPublishedAt(),feedCache.lastDeliveredIndex(),feedCache.lastDeliveredPublishedAt());
     }
 
     public void saveFeedCache(String cacheKey, List<NewsInfo> articles){
         FeedCache oldCache = getFeedCache(cacheKey);
-        saveFeedCache(cacheKey, articles, oldCache.lastDeliveredIndex());
+        saveFeedCache(cacheKey, articles, oldCache.lastDeliveredIndex(),oldCache.lastDeliveredPublishedAt());
     }
 
 
-    public void saveFeedCache(String cacheKey,List<NewsInfo> articles, Integer lastDeliveredIndex){
+    public void saveFeedCache(String cacheKey,List<NewsInfo> articles, Integer lastDeliveredIndex, String lastDeliveredPublishedAt){
         Cache cache = cacheManager.getCache("generalFeed");
-        if(cache == null)return;
-        //Losiento x2
-        List<NewsInfo> savedArticles = cleanAndSortArticles(articles);
-        FeedCache feedCache = new FeedCache(new ArrayList<>(savedArticles),findNewestPublishedAt(savedArticles), findOldestPublishedAt(savedArticles),lastDeliveredIndex);
-        cache.put(cacheKey, feedCache);
+        if(cache != null) {
+            List<NewsInfo> savedArticles = cleanAndSortArticles(articles);
+            FeedCache feedCache = new FeedCache(new ArrayList<>(savedArticles), findNewestPublishedAt(savedArticles), findOldestPublishedAt(savedArticles), lastDeliveredIndex, lastDeliveredPublishedAt);
+            cache.put(cacheKey, feedCache);
+        }
 
     }
     public List<NewsInfo> removeAlreadyCachedArticles(List<NewsInfo> cachedArticles, List<NewsInfo> newArticles) {
@@ -288,8 +305,24 @@ public class NewsInfoService {
 
         return newArticles.stream()
                 .filter(article -> article.getUrl() != null && !article.getUrl().isBlank())
-                .filter(article -> !cachedUrls.contains(article.getUrl()))
+                .filter(article -> !cachedUrls.contains(getArticleUniqueKey(article)) )
                 .toList();
+    }
+
+    private String getArticleUniqueKey(NewsInfo article) {
+        if (article == null) {
+            return "";
+        }
+
+        String title = article.getTitle() != null
+                ? article.getTitle().trim().toLowerCase()
+                : "";
+
+        String publishedAt = article.getPublishedAt() != null
+                ? article.getPublishedAt().trim()
+                : "";
+
+        return title + "|" + publishedAt;
     }
 
 
@@ -298,7 +331,9 @@ public class NewsInfoService {
             return Collections.emptyList();
         }
 
-        return articles.stream().filter(article -> article.getUrl() != null && !article.getUrl().isBlank())
+        return articles.stream()
+                .filter(article -> article.getUrl() != null && !article.getUrl().isBlank())
+                .filter(article -> article.getTitle() != null && !article.getTitle().isBlank())
                 .collect(Collectors.toMap(
                         NewsInfo::getUrl,
                         article -> article,
@@ -311,7 +346,7 @@ public class NewsInfoService {
                         NewsInfo::getPublishedAt,
                         Comparator.nullsLast(Comparator.reverseOrder())
                 ))
-                .toList();
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
 
