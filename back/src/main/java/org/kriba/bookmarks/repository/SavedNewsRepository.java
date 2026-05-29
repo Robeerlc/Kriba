cf8ec0f377068ac0f4a1723032a49948c601d71b
GuillermoDelgado24 <guillermo.delgado545@gmail.com>
2026-05-29 11:46:48 +0200

    Update SavedNewsRepository.java

--- back/src/main/java/org/kriba/bookmarks/repository/SavedNewsRepository.java
+++ back/src/main/java/org/kriba/bookmarks/repository/SavedNewsRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import java.util.List;
 
 public interface SavedNewsRepository extends JpaRepository<SavedArticle, Long> {
     Page<SavedArticle> findAllByUserId(Long userid, Pageable pageable);
