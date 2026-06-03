6637f38259ab3d0a9ea6e4391d3c243982ee6ec0
GuillermoDelgado24 <guillermo.delgado545@gmail.com>
2026-06-03 08:54:24 +0200

    Añadido de cacheable

--- back/src/main/java/org/kriba/news/dto/FeedCache.java
+++
@@ -1,6 +0,0 @@
-package org.kriba.news.dto;
-
-import java.util.List;
-
-public record FeedCache(List<NewsInfo> articles, String newestPublishedAt, String oldestPublishedAt, Integer lastDeliveredIndex) {
-}
